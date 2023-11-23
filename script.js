2727807bbc88593497e04401ed7831935e0abf0a
joyrss <95247539+joyrss@users.noreply.github.com>
2023-11-23 16:51:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 29, 2023 15:37:25").getTime();
+var countDownDate = new Date("Dec 29, 2023 18:30:00").getTime();
 
 var x = setInterval(function() {
 var now = new Date().getTime();
