d57dd8f8c3da5dfd6f3960f55c24c57b970d7a82
joyrss <95247539+joyrss@users.noreply.github.com>
2023-12-29 10:43:31 +0200

    Update insta.js

--- insta.js
+++ insta.js
@@ -1,25 +1,43 @@
-const hashtag = 'thepapwedding'; // Replace with your desired hashtag
-        const endpoint = `https://www.instagram.com/explore/tags/${hashtag}/?__a=1`;
+// Replace 'YOUR_ACCESS_TOKEN' with your actual Instagram Graph API Access Token
+const accessToken = 'IGQWRNNURIRUh6a1RQZAVk4NU5LcUtsc3FKOGlUUF9rMjU4S3V0ZAFo2QlZAQT1ZAMWERlZAkpHVWo0Um93UjhldW9FLUVnVFpsZAk1PU0tFN2xNd2dpVlJuRC1ybGx1djRDQjJHSzBWWkVwTG0yUmEwTE5wUGUzR2U1dmcZD';
+// Replace 'YOUR_HASHTAG' with the hashtag you want to display
+const hashtag = 'thepapwedding';
 
-        fetch(endpoint)
-            .then(response => response.json())
-            .then(data => {
-                const posts = data.graphql.hashtag.edge_hashtag_to_media.edges;
+// Instagram Graph API endpoint for hashtag search
+const apiEndpoint = `https://graph.instagram.com/v13.0/ig_hashtag_search?q=${hashtag}&access_token=${accessToken}`;
 
-                // Iterate through posts and display them
-                posts.forEach(post => {
-                    const imageUrl = post.node.display_url;
-                    const caption = post.node.edge_media_to_caption.edges[0]?.node.text;
+// Function to get recent posts for a hashtag
+function getHashtagPosts(hashtagId) {
+    const postsEndpoint = `https://graph.instagram.com/v13.0/${hashtagId}/recent_media?access_token=${accessToken}`;
+    $.get(postsEndpoint, (data) => {
+        displayPosts(data.data);
+    });
+}
 
-                    // Display the image and caption as you like
-                    const imageElement = document.createElement('img');
-                    imageElement.src = imageUrl;
+// Function to display Instagram posts
+function displayPosts(posts) {
+    const galleryContainer = $('#instagram-gallery');
+    
+    // Loop through each post and display its image
+    posts.forEach(post => {
+        const imageUrl = post.media_url;
+        const imageElement = `<img src="${imageUrl}" alt="Instagram Post" style="width: 200px; height: 200px; margin: 5px;">`;
+        galleryContainer.append(imageElement);
+    });
+}
 
-                    const captionElement = document.createElement('p');
-                    captionElement.textContent = caption;
+// Function to initialize the Instagram hashtag gallery
+function initInstagramGallery() {
+    // Get the hashtag ID
+    $.get(apiEndpoint, (data) => {
+        if (data.data.length > 0) {
+            const hashtagId = data.data[0].id;
+            getHashtagPosts(hashtagId);
+        } else {
+            console.error('Hashtag not found.');
+        }
+    });
+}
 
-                    document.body.appendChild(imageElement);
-                    document.body.appendChild(captionElement);
-                });
-            })
-            .catch(error => console.error('Error fetching Instagram data:', error));
+// Call the function to initialize the Instagram gallery
+initInstagramGallery();
